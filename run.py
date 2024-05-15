a409eab93f20ac8476148741afc42842393b55d5
Henrik Andersson <henrik.andersson244@gmail.com>
2024-05-15 17:27:10 +0000

    Added spaces to make file more unison and better code readability

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 from questions import nfl_questions
 
+
 #The scope below lists the APIs that my program should access in order to run
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -15,15 +16,18 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
+
 #Eveything to do with google credentials below are copied from Code Institute Love Sandwiches project
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('NFL_Quiz')
 
+
 #This function adds the ability to clear the terminal
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
+
 
 # Function to start the game, 
 def game_start():
